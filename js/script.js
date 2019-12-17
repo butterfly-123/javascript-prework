a402d1e1794a7b7693e4c46bfcf0882ed1bde853
Bombel <bombel@bombel.com>
2019-12-17 20:27:36 +0100

    Add Math.random()

--- js/script.js
+++ js/script.js
@@ -13,3 +13,7 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba to: ' + randomNumber);
