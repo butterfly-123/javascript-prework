899a437faedd6ba27db2764f003e963148dd3b3d
Bombel <bombel@bombel.com>
2019-12-17 23:01:05 +0100

    Add random number by the two players

--- js/script.js
+++ js/script.js
@@ -23,9 +23,23 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
     computerMove = 'kamień';
-} else if (randomNumber == 2) {
+} else if (randomNumber == '2') {
     computerMove = 'papier';
-} else if (randomNumber == 3) {
-    computerMove = 'nozyce';
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce'
+} else (playerInput == '') {
+    playerMove = prompt('Wybrałeś niewłaściwy numer! Spróbuj jeszcze raz.');
+}
+printMessage('Twój ruch: ' + playerMove);
