8d55311361690be1dba28ab2627583c4edacac9f
Bombel <bombel@bombel.com>
2019-12-17 22:35:22 +0100

    Add random number by the player

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to prz
 var randomNumber;
 randomNumber = Math.floor(Math.random() * 9 + 11);
 printMessage('Wylosowana liczba to: ' + randomNumber);
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'noyce';
+}
+printMessage('Mój ruch: ' + computerMove);
